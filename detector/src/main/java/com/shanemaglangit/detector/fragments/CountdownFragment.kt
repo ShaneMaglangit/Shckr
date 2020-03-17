6d12bf71adc3cba0e4fab7537219a3582c837395
ShaneMaglangit <shanedmaglangit@gmail.com>
2020-03-17 21:10:20 +0800

    Added countdown viewBinding

--- detector/src/main/java/com/shanemaglangit/detector/fragments/CountdownFragment.kt
+++ detector/src/main/java/com/shanemaglangit/detector/fragments/CountdownFragment.kt
@@ -6,18 +6,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.shanemaglangit.detector.R
+import com.shanemaglangit.detector.databinding.FragmentCountdownBinding
 
-/**
- * A simple [Fragment] subclass.
- */
 class CountdownFragment : Fragment() {
+    private lateinit var binding: FragmentCountdownBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_countdown, container, false)
+        binding = FragmentCountdownBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
 }
